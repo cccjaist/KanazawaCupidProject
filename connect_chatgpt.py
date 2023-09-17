97544a80c9e903000c8b91b036739cd7129dc2b7
makabi-bmk <nojiko0101@gmail.com>
2023-09-18 03:24:35 +0900

    chatGPTにアクセスして話すまでのプログラムを書いた

--- connect_chatgpt.py
+++ connect_chatgpt.py
@@ -1,12 +1,31 @@
-ENV_KEY_NAME = 'OPENAI_API_KEY'
+OPENAI_API_KEY = 'OPENAI_API_KEY'
+OPENAI_ORGANIZATION = 'OPENAI_ORGANIZATION'
+CHATGPT_MODEL = 'gpt-3.5-turbo'
 
 import os
 import openai
 from dotenv import load_dotenv
 
-def init_chatgpt():
+def init():
     # envファイルからchatGPTのAPIキーを読み込む
     load_dotenv()
-    openai.api_key = os.environ[ENV_KEY_NAME]
+    openai.api_key = os.environ[OPENAI_API_KEY]
+    openai.organization = os.environ[OPENAI_ORGANIZATION]
 
-    print(os.environ[ENV_KEY_NAME])
+def get_response(text):
+
+    res = openai.ChatCompletion.create(
+        model = CHATGPT_MODEL,
+        messages = [
+            {
+                'role': 'user',
+                'content': text
+            }
+        ]
+    )
+
+    # TODO : クレジットの登録が必要
+    # URL: https://qiita.com/kotattsu3/items/d6533adc785ee8509e2c
+    ans = res.choices[0].text.strip()
+    return ans
+
