361c7bc4c5fa2fdf01bce190bee8493f8a337e66
makabi-bmk <nojiko0101@gmail.com>
2023-09-18 01:34:14 +0900

    VOICEVOXを使った音声読み上げ機能を追加した #1

--- main.py
+++ main.py
@@ -2,9 +2,13 @@ import my_speech_recognition as my_sr
 
 def main():
     while True:
-        # 音声をテキスト情報に出力
+        # 音声をテキスト情報に変換
         text = my_sr.get_speech_recognize()
         print(text)
+
+        filename = 'audio.wav'  # 音声データのファイル名
+        my_sr.text_2_wav(text, filename=filename)
+        my_sr.play_auido_by_filename(filename)
 
 if __name__ == '__main__':
     main()
