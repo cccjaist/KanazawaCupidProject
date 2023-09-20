d8bb7b87102c75d41ea3c19470c9c16b1020fbeb
makabi-bmk <nojiko0101@gmail.com>
2023-09-20 23:13:00 +0900

    htmlとserverのフォルダを作成

--- test.py
+++ test.py
@@ -1,22 +1,7 @@
-import json
-import requests
-import simpleaudio
-import speech_recognition as sr
+from flask import Flask
+app = Flask(__name__, static_folder='.', static_url_path='')
+@app.route('/')
+def index():
+    return app.send_static_file('index.html')
 
-while True:
-    # 音声入力
-    recognizer = sr.Recognizer()
-
-    text = ''
-    print('何か話してください…')
-    with sr.Microphone() as source:
-        audio = recognizer.listen(source)
-
-    try:
-        # Google Web Speech APIで音声認識
-        text = recognizer.recognize_google(audio, language='ja-JP')
-    except sr.UnknownValueError:
-        print("Google Web Speech APIは音声を認識できませんでした。")
-    except sr.RequestError as e:
-        print("GoogleWeb Speech APIに音声認識を要求できませんでした;"
-            " {0}".format(e))
+app.run(port=8000, debug=True)
