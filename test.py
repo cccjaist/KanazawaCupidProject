ee34386004d7a46e89945ed4b22148ad5c651a5b
makabi-bmk <nojiko0101@gmail.com>
2023-09-20 23:31:16 +0900

    fletのインストール

--- test.py
+++ test.py
@@ -1,7 +1,6 @@
-from flask import Flask
-app = Flask(__name__, static_folder='.', static_url_path='')
-@app.route('/')
-def index():
-    return app.send_static_file('index.html')
+import flet as ft
 
-app.run(port=8000, debug=True)
+def main(page: ft.Page):
+    pass
+
+ft.app(target=main)
