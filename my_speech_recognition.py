c103530005793d374cc650d6ffbbf9919f4b2e85
makabi-bmk <nojiko0101@gmail.com>
2023-10-02 20:20:15 +0900

    localhostをipアドレスに変更した
    名前解決の時間が減って速度が解決するらしい

--- my_speech_recognition.py
+++ my_speech_recognition.py
@@ -41,9 +41,9 @@ def recognize(audio, log):
 # TODO: spkaker_id用の設定ファイル作る
 def text_2_wav(text, log, speaker_id=8, max_retry=20, filename='audio.wav'):
     # 音声合成のための、クエリを作成
-    query_payload = {"text": text, "speaker": speaker_id}
+    query_payload = {'text': text, 'speaker': speaker_id}
     for query_i in range(max_retry):
-        response = requests.post("http://localhost:50021/audio_query",
+        response = requests.post('http://127.0.0.1:50021/audio_query',
                                  params=query_payload,
                                  timeout=10)
         if response.status_code == 200:
@@ -53,14 +53,14 @@ def text_2_wav(text, log, speaker_id=8, max_retry=20, filename='audio.wav'):
         log.write_error_log('リトライ回数が上限に到達しました。')
 
     # 音声合成データの作成して、wavファイルに保存
-    synth_payload = {"speaker": speaker_id}
+    synth_payload = {'speaker': speaker_id}
     for synth_i in range(max_retry):
-        response = requests.post("http://localhost:50021/synthesis",
+        response = requests.post('http://127.0.0.1:50021/synthesis',
                                  params=synth_payload,
                                  data=json.dumps(query_data),
                                  timeout=100)
         if response.status_code == 200:
-            with open(filename, "wb") as fp:
+            with open(filename, 'wb') as fp:
                 fp.write(response.content)
             break
     else:
